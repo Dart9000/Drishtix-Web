8b5fbbd9ea6c0967ca41e3af894d874e7d0cdfbd
sourabhjo7 <sourabhjoshi9549408165@gmail.com>
2022-06-19 19:07:09 +0530

    react login functionality and protected route added

--- client/src/App.js
+++ client/src/App.js
@@ -2,31 +2,68 @@ import './App.css';
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
-import { useState } from 'react';
+import { useState ,useEffect} from 'react';
+import axios from "axios";
 
-let isauth;
 function App() {
  const[auth,setauth] =useState(false);
+ const[admin,setadmin]=useState(false);
+
+ useEffect(() => {
+  validate();
+ }, []);
  
+ const validate= async()=>{
+  const result=await axios.post("http://localhost:3002/api/user/login",{"name":"user",
+  "email":"user@gmail.com",
+  "password":"user123"});
+
+  if(result.status==200){
+    setauth(true);
+  }
+  else{
+    setauth(false);
+  }
+  if(result.data.Admin){
+    setadmin(true);
+  }
+  else{
+    setauth(false);
+  }
+  
+ }
+
  
  return (
     <div className="App">
-      hiii welcome 
       <Routes>
+
         <Route path="/" element={
             <ProtectedRoute originurl={"/"} isauth={auth}>
               <Home />
             </ProtectedRoute>
           } />
+
         <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
 }
 
-// component to protect routes
+// component to protect routes for user privilige
 const ProtectedRoute = ({originurl,isauth,children})=>{
   if(isauth){
+    return children;
+  }
+  else{
+    return <Navigate to="/login" replace />
+  }
+
+}
+
+// component to protect routes for admin privilege
+const AdminProtectedRoute = ({originurl,isadmin,children})=>{
+  if(isadmin){
     return children;
   }
   else{
