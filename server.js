20a3690689877967cb258a943f855c1b9b5d3a96
sourabhjo7 <sourabhjoshi9549408165@gmail.com>
2022-06-19 15:13:45 +0530

    testing done

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
-const {userRoutes} =require("./routes/userRoutes");
+const userRoutes =require("./routes/userRoutes");
 const app = express()
 const cors=require("cors");
 const mongoose=require("mongoose");
