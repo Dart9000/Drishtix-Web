860abe25fc2dd2b2498bd7efaa565640ba94a0b5
Mrityunjay383 <mrityunjayvyasmhl@gmail.com>
2022-06-21 00:00:42 +0530

    LiveCam

--- client/src/pages/LiveCam/LiveCam.js
+++ client/src/pages/LiveCam/LiveCam.js
@@ -1,26 +1,44 @@
 import React, { useState, useRef, useCallback, useEffect } from "react";
 import Webcam from "react-webcam";
+import axios from "axios";
+
+const cctv = {
+  phone: "8949190774",
+  address: "Bhilwara"
+}
 
 function LiveCom() {
   const FACING_MODE_USER = "user";
   const FACING_MODE_ENVIRONMENT = "environment";
 
   const webcamRef = useRef(null);
-  const [image, setImage] = useState("");
 
   const videoConstraints = {
     width: 220,
     height: 200
   };
 
-  const capture = useCallback(() => {
+  const capture = useCallback(async () => {
     const imageSrc = webcamRef.current.getScreenshot();
-    setImage(imageSrc);
+    console.log(imageSrc);
+
+    const exData = new FormData();
+    exData.append("file", imageSrc);
+    await axios.post(`https://drishtix-api.herokuapp.com/search?Phone=${cctv.phone}&Address=${cctv.address}`, exData, {
+      validateStatus: false,
+      withCredentials: true
+    },{
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then((response) => {
+      console.log(response);
+    });
   }, [webcamRef]);
 
 
   useEffect(() => {
-    capture();
+    setInterval(capture, 5000);
   }, [])
 
   return (
